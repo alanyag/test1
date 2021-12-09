33fb07ecdffb693b589022862f1ac0cd619374e6
alanyag <95857566+alanyag@users.noreply.github.com>
2021-12-09 20:25:41 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 w = st.number_input('請輸入體重(KG)？')
-h = st.number_input('input('請輸入身高(M)？')
+h = st.number_input('請輸入身高(M)？')
 comfirm_imput = st.buttom('輸入確認')
 
 if comfirm_input:
         bmi = w/(h*h)
-        st.write('BMI為',bmi
+        st.write('BMI為',bmi)
         if (bmi < 18):
             st.print('體重過輕')
         elif (bmi < 24):
