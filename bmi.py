10cfa70bdefaa87e214db057b283c15c2cc05f25
alanyag <95857566+alanyag@users.noreply.github.com>
2021-12-09 20:17:49 +0800

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+w = st.number_input('請輸入體重(KG)？')
+h = st.number_input(input('請輸入身高(M)？')
+comfirm_imput = st.buttom('輸入確認)
+
+if comfirm_input:
+        bmi = BMI(w, h)
+        if (bmi < 18):
+            st.print('體重過輕')
+        elif (bmi < 24):
+            st.print('體重正常')
+        elif (bmi < 27):
+            st.print('體重過重')
+        else:
+            st.print('體重肥胖')
