3d862d694d9e449249d6f39eefa7c64bd830697b
alanyag <95857566+alanyag@users.noreply.github.com>
2021-12-09 20:23:47 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 w = st.number_input('請輸入體重(KG)？')
-h = st.number_input(input('請輸入身高(M)？')
-comfirm_imput = st.buttom('輸入確認)
+h = st.number_input('input('請輸入身高(M)？')
+comfirm_imput = st.buttom('輸入確認')
 
 if comfirm_input:
         bmi = w/(h*h)
